85757782871125a11aa26df670f5351077bb475d
matheus <matheusdev20@gmail.com>
2024-01-07 00:55:26 -0300

    feat: changing logo design

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,9 +1,13 @@
-import HRLOGO from '../../assets/imgs/hr_max_logo.png'
+import PEOPLE_PILOT_LOGO from '../../assets/imgs/people_pilot_logo.png'
 
 export const Logo = (): JSX.Element => {
   return (
     <div className="flex flex-col gap-3">
-      <img src={HRLOGO} alt="logo" className="object-cover w-40 h-40" />
+      <img
+        src={PEOPLE_PILOT_LOGO}
+        alt="logo"
+        className="object-cover w-40 h-40"
+      />
     </div>
   )
 }
