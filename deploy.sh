64bf4ffb403d93c581ab963a7154b9212794ba52
matheus <matheusdev20@gmail.com>
2024-01-07 00:57:13 -0300

    chore: changing deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@ echo 'Starting building...'
 
 npm run build
 
+echo 'Done!'
+
+echo 'Moving files via secure shell ...'
+
 scp -r  ./dist/* root@204.48.26.219:/var/www/peoplepilot.com.br/html 
 
+echo 'Done!'
 echo 'Deployed successfully!'
